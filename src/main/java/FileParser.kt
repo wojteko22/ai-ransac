b00b2b8a0c52ac834bf5dfe99fd22bfd3dad54bc
Wojtek Okoński <wojteko22.96@gmail.com>
2018-05-16 16:44:13 +0200

    Add some lines

--- src/main/java/FileParser.kt
+++ src/main/java/FileParser.kt
@@ -4,8 +4,11 @@ const val featuresCount = 128
 
 fun main(args: Array<String>) {
     val rootPath = "src/main/resources"
-    val keyPoints1 = keyPoints("$rootPath/1.png.haraff.sift")
-    val keyPoints2 = keyPoints("$rootPath/2.png.haraff.sift")
+    val firstFileName = "DSC_5824"
+    val secondFileName = "DSC_5825"
+    val keyPoints1 = keyPoints("$rootPath/$firstFileName.png.haraff.sift")
+    val keyPoints2 = keyPoints("$rootPath/$secondFileName.png.haraff.sift")
+    println("$firstFileName $secondFileName")
     val indexes1 = keyPoints1.map { findClosestIndex(it, keyPoints2) }
     val indexes2 = keyPoints2.map { findClosestIndex(it, keyPoints1) }
     indexes1.forEachIndexed { index, indexOfClosest ->
@@ -37,9 +40,11 @@ private fun keyPoints(pathname: String): List<KeyPoint> {
     val subList = lines.subList(2, lines.size)
     return subList.map {
         val itemsInLine = it.split(' ')
+        val x = itemsInLine[0].toDouble()
+        val y = itemsInLine[1].toDouble()
         val features = itemsInLine.subList(5, itemsInLine.size).map { it.toInt() }
-        KeyPoint(features)
+        KeyPoint(x, y, features)
     }
 }
 
-class KeyPoint(val features: List<Int>)
+class KeyPoint(val x: Double, val y: Double, val features: List<Int>)
