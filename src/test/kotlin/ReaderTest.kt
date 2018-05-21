b3b137028067dcb0098c54fedfd0ca8711d861d9
Wojtek Okoński <wojteko22.96@gmail.com>
2018-05-21 16:14:09 +0200

    Add test skeleton

---
+++ src/test/kotlin/ReaderTest.kt
@@ -0,0 +1,10 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class ReaderTest {
+
+    @Test
+    fun keyPoints() {
+        assertThat(1).isEqualTo(1)
+    }
+}
