9cd856439b63d0692cec6ab61f002cf719164342
Wojtek Okoński <wojteko22.96@gmail.com>
2018-05-21 16:26:48 +0200

    Implement test

--- src/main/kotlin/KeyPoint.kt
+++ src/main/kotlin/KeyPoint.kt
@@ -1,1 +1,1 @@
-class KeyPoint(val x: Double, val y: Double, val features: List<Int>)
+data class KeyPoint(val x: Double, val y: Double, val features: List<Int>)
--- src/test/kotlin/ReaderTest.kt
+++ src/test/kotlin/ReaderTest.kt
@@ -5,6 +5,14 @@ internal class ReaderTest {
 
     @Test
     fun keyPoints() {
-        assertThat(1).isEqualTo(1)
+        val expectedKeyPoints = listOf(
+            KeyPoint(0.0, 0.0, listOf(1, 2)),
+            KeyPoint(0.0, 0.0, listOf(3, 4))
+        )
+        val reader = Reader()
+
+        val keyPoints = reader.keyPoints("src/test/resources/test1.png.haraff.sift")
+
+        assertThat(keyPoints).isEqualTo(expectedKeyPoints)
     }
 }
