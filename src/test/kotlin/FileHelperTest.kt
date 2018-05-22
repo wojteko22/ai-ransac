b9778dc7303d7d67f5607ca5f736aa76a1fe1dcc
Wojtek Okoński <wojteko22.96@gmail.com>
2018-05-22 17:30:47 +0200

    Test serializing

--- src/test/kotlin/FileHelperTest.kt
+++ src/test/kotlin/FileHelperTest.kt
@@ -1,18 +1,38 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.io.File
 
 internal class FileHelperTest {
 
+    private val rootPathname = "src/test/resources"
+    private val reader = FileHelper(rootPathname)
+
     @Test
     fun keyPoints() {
-        val expectedPicture = Picture(listOf(
-            KeyPoint(0.0, 0.0, listOf(1, 2)),
-            KeyPoint(0.0, 0.0, listOf(3, 4))
-        ))
-        val reader = FileHelper("src/test/resources")
+        val expectedPicture = Picture(
+            listOf(
+                KeyPoint(0.0, 0.0, listOf(1, 2)),
+                KeyPoint(0.0, 0.0, listOf(3, 4))
+            )
+        )
 
         val picture = reader.keyPoints("test1.png.haraff.sift")
 
         assertThat(picture).isEqualTo(expectedPicture)
+    }
+
+    @Test
+    fun save() {
+        val pathname = "auto-deleting.json"
+        val content = listOf("a", "b", "c")
+        val file = File("$rootPathname/$pathname")
+        val expectedJson = File("$rootPathname/expected.json").readText()
+
+        reader.save(pathname, content)
+        val json = file.readText()
+
+        assertThat(json).isEqualToIgnoringNewLines(expectedJson)
+
+        file.delete()
     }
 }
