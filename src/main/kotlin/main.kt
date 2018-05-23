1f66274879df7b865686e056d126f4d743ef2262
Wojtek Okoński <wojteko22.96@gmail.com>
2018-05-23 10:32:28 +0200

    Generate consistent pairs

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,7 +5,7 @@ private const val pairsPathname = "pairs2.json"
 fun main(args: Array<String>) {
     val io = FileHelper()
     val millis = measureTimeMillis {
-//        io.savePairs("DSC_5824.png.haraff.sift", "DSC_5825.png.haraff.sift", pairsPathname)
+        io.savePairs("DSC_5824.png.haraff.sift", "DSC_5825.png.haraff.sift", pairsPathname)
         io.saveConsistentPairs(pairsPathname, "consistent2.json", 4, 0.6)
     }
     println("millis: $millis")
