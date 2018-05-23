f685d27c609cabfdcffef138729e04d83e2f3762
Wojtek Okoński <wojteko22.96@gmail.com>
2018-05-23 11:49:07 +0200

    Refactor main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,10 +2,14 @@ import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
-import kotlin.system.measureTimeMillis
 
+private const val neighborhoodSize = 4
+private const val threshold = 0.6
 private const val pairsPathname = "pairs2.json"
+private const val consistentPairsPathname = "consistent2,n=$neighborhoodSize,t=$threshold.json"
+
 private const val resourcesPathname = "src/main/resources"
+private val io = PairsIOExecutor(resourcesPathname)
 
 fun main(args: Array<String>) {
     doSomethingWithPairs()
@@ -13,12 +17,8 @@ fun main(args: Array<String>) {
 }
 
 private fun doSomethingWithPairs() {
-    val io = PairsIOExecutor(resourcesPathname)
-    val millis = measureTimeMillis {
-        io.savePairs("DSC_5824.png.haraff.sift", "DSC_5825.png.haraff.sift", pairsPathname)
-        io.saveConsistentPairs(pairsPathname, "consistent2.json", 4, 0.6)
-    }
-    println("millis: $millis")
+    io.savePairs("DSC_5824.png.haraff.sift", "DSC_5825.png.haraff.sift", pairsPathname)
+    io.saveConsistentPairs(pairsPathname, consistentPairsPathname, neighborhoodSize, threshold)
 }
 
 private fun doSomethingWithImages() {
