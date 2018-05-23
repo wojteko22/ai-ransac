ad7daa901ebede5cacc8e06a0fc3498f09f310e4
Wojtek Okoński <wojteko22.96@gmail.com>
2018-05-23 14:14:35 +0200

    Refactor main and files' names

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,18 +3,15 @@ private const val threshold = 0.6
 private const val pairsPathname = "pairs2.json"
 private const val image1 = "DSC_5824.png"
 private const val image2 = "DSC_5825.png"
-private const val resultImage = "my.png"
-private const val consistentPairsPathname = "consistent2,n=$neighborhoodSize,t=$threshold.json"
+private const val consistentPairsDescription = "consistent2,n=$neighborhoodSize,t=$threshold"
+private const val consistentPairsPathname = "$consistentPairsDescription.json"
+private const val resultImage = "$consistentPairsDescription.png"
 
 private const val resourcesPathname = "src/main/resources"
 private val executor = OperationExecutor(resourcesPathname)
 
 fun main(args: Array<String>) {
-    doSomethingWithPairs()
+//    executor.savePairs("$image1.haraff.sift", "$image2.haraff.sift", pairsPathname)
+//    executor.saveConsistentPairs(pairsPathname, consistentPairsPathname, neighborhoodSize, threshold)
     executor.drawLines(image1, image2, resultImage, consistentPairsPathname)
-}
-
-private fun doSomethingWithPairs() {
-    executor.savePairs("$image1.haraff.sift", "$image2.haraff.sift", pairsPathname)
-    executor.saveConsistentPairs(pairsPathname, consistentPairsPathname, neighborhoodSize, threshold)
 }
